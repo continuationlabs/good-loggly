03701bd145991a70d1abdd8d1266bb2eacb8af3c
Frederic Hemberger <mail@frederic-hemberger.de>
2014-11-25 15:16:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# good-loggly
+
+Loggly broadcasting for Good process monitor
+
+[![Build Status](https://travis-ci.org/fhemberger/good-loggly.svg?branch=master)](http://travis-ci.org/fhemberger/good-loggly)![Current Version](https://img.shields.io/npm/v/good-loggly.svg)
+
+
+## Usage
+
+`good-loggly` is a [good-reporter](https://github.com/hapijs/good-reporter) implementation to write [hapi](http://hapijs.com/) server events to Loggly.
+
+Example integration:
+```javascript
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+var options = {
+    reporters: [{
+        reporter: require('good-loggly'),
+        args: [
+            { log: '*', request: '*'},
+            {
+                // Required
+                token     : 'YOUR LOGGLY TOKEN',
+                subdomain : 'YOUR LOGGLY SUBDOMAIN',
+                // Optional
+                name      : 'myapp',
+                hostname  : 'myapp.example.com'
+            }
+        ]
+    }]
+};
+
+server.pack.register(
+    {
+        plugin: require('good'),
+        options: options
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    }
+);
+
+```
+
+
+## License
+
+[MIT](LICENSE)
---
+++ test/index.js
@@ -0,0 +1,85 @@
+// Load modules
+var EventEmitter = require('events').EventEmitter;
+var proxyquire   = require('proxyquire');
+var hoek         = require('hoek');
+var Lab          = require('lab');
+var Code         = require('code');
+
+var logglyStub = {
+    createClient: function() {},
+    log: function(event, tags) { process.exit(); }
+};
+var GoodLoggly = proxyquire('..', { 'loggly': logglyStub });
+
+
+// Declare internals
+var internals = {};
+
+internals.logglyOptions = {
+    token     : 'TOKEN',
+    subdomain : 'SUBDOMAIN'
+};
+
+internals.logEventData = {
+    event     : 'log',
+    timestamp : 1396207735000,
+    tags      : ['info', 'server'],
+    data      : 'Log message',
+    pid       : 1234
+};
+
+internals.logglyResult = {
+    event     : 'log',
+    timestamp : '2014-03-30T21:28:55.000Z',
+    tags      : ['info', 'server'],
+    data      : 'Log message',
+    msg       : 'Log message',
+    pid       : 1234
+};
+
+
+// Test shortcuts
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var expect = Code.expect;
+
+
+describe('GoodLoggly', function () {
+    it('should throw an error if not constructed with new', function (done) {
+        expect(function () {
+            var reporter = GoodLoggly();
+        }).to.throw('GoodLoggly must be created with new');
+        done();
+    });
+
+    describe('_timeString()', function () {
+        it('should formats the time as ISO 8601 date', function (done) {
+            var time = new Date(1396207735000);
+            var result = GoodLoggly.timeString(time);
+
+            expect(result).to.equal('2014-03-30T21:28:55.000Z');
+            done();
+        });
+    });
+
+    describe('_report()', function () {
+
+        it('should throw an error for ops events', function (done) {
+            expect(function () {
+                var reporter = new GoodLoggly({ ops: '*' }, internals.logglyOptions);
+            }).to.throw('"ops" events are not supported by Loggly');
+            done();
+        });
+
+        it('should log an event', function (done) {
+            var reporter = new GoodLoggly({ test: '*' }, internals.logglyOptions);
+            var result = reporter._report('log', internals.logEventData);
+
+            expect(result).to.deep.equal(internals.logglyResult);
+            done();
+        });
+    });
+});
