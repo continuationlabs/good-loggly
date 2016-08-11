6f3ef6cbc791d51148f7d48b32a60607dd200501
Brandon Belvin <brandon@sharpcoffee.com>
2016-08-11 14:51:46 -0500

    Add bulk messaging support (#19)
    Closes #18

--- test/index.js
+++ test/index.js
@@ -20,6 +20,26 @@ const logEventData = {
   pid: 1234
 };
 
+const bulkLogEventData = [{
+  event: 'log',
+  timestamp: 1396207735000,
+  tags: ['info', 'server'],
+  data: 'Log message',
+  pid: 1234
+}, {
+  event: 'log',
+  timestamp: 1396207846000,
+  tags: ['info', 'app'],
+  data: 'New log message',
+  pid: 1234
+}, {
+  event: 'log',
+  timestamp: 1396207957000,
+  tags: ['info', 'app2'],
+  data: 'Newer log message',
+  pid: 1234
+}];
+
 const logglyResult = {
   event: 'log',
   timestamp: new Date(1396207735000).toISOString(),
@@ -28,6 +48,38 @@ const logglyResult = {
   msg: 'Log message',
   pid: 1234
 };
+
+const bulkResult = [{
+  data: 'Log message',
+  event: 'log',
+  msg: 'Log message',
+  pid: 1234,
+  tags: [
+    'info',
+    'server'
+  ],
+  timestamp: '2014-03-30T19:28:55.000Z'
+}, {
+  data: 'New log message',
+  event: 'log',
+  msg: 'New log message',
+  pid: 1234,
+  tags: [
+    'info',
+    'app'
+  ],
+  timestamp: '2014-03-30T19:30:46.000Z'
+}, {
+  data: 'Newer log message',
+  event: 'log',
+  msg: 'Newer log message',
+  pid: 1234,
+  tags: [
+    'info',
+    'app2'
+  ],
+  timestamp: '2014-03-30T19:32:37.000Z'
+}];
 
 const optionalFields = {
   name: 'loggly-name',
@@ -153,6 +205,115 @@ describe('GoodLoggly', () => {
 
       expect(result).to.equal('');
       done();
+    });
+  });
+
+  describe('bulk', () => {
+    it('holds messages until threshold is met', (done) => {
+      const stream = readStream();
+      const options = Merge({ threshold: 2, maxDelay: 0 }, logglyOptions, optionalFields);
+      const reporter = new GoodLoggly(options);
+
+      Loggly.Loggly.prototype.log = function (event, tags, callback) {
+        expect(event).to.be.an.array();
+        expect(event).to.have.length(2);
+        expect(event[0]).to.equal(Merge({}, bulkResult[0], optionalFields));
+        expect(event[1]).to.equal(Merge({}, bulkResult[1], optionalFields));
+        expect(tags).to.be.a.function();
+        expect(callback).to.be.undefined();
+        done();
+      };
+
+      stream.pipe(reporter);
+      stream.push(bulkLogEventData[0]);
+      stream.push(bulkLogEventData[1]);
+    });
+
+    it('releases after maxDelay if threshold is not met', (done) => {
+      const stream = readStream();
+      const options = Merge({ threshold: 100, maxDelay: 150 }, logglyOptions, optionalFields);
+      const reporter = new GoodLoggly(options);
+
+      Loggly.Loggly.prototype.log = function (event, tags, callback) {
+        expect(event).to.be.an.array();
+        expect(event).to.have.length(3);
+        expect(event[0]).to.equal(Merge({}, bulkResult[0], optionalFields));
+        expect(event[1]).to.equal(Merge({}, bulkResult[1], optionalFields));
+        expect(event[2]).to.equal(Merge({}, bulkResult[2], optionalFields));
+        expect(tags).to.be.a.function();
+        expect(callback).to.be.undefined();
+        done();
+      };
+
+      stream.pipe(reporter);
+      stream.push(bulkLogEventData[0]);
+      stream.push(bulkLogEventData[1]);
+      setTimeout(() => {
+        stream.push(bulkLogEventData[2]);
+      }, 200);
+    });
+
+    it('clears the buffer on termination of stream', (done) => {
+      const stream = readStream();
+      const options = Merge({ threshold: 100 }, logglyOptions, optionalFields);
+      const reporter = new GoodLoggly(options);
+
+      Loggly.Loggly.prototype.log = function (event, tags, callback) {
+        expect(event).to.be.an.array();
+        expect(event).to.have.length(3);
+        expect(event[0]).to.equal(Merge({}, bulkResult[0], optionalFields));
+        expect(event[1]).to.equal(Merge({}, bulkResult[1], optionalFields));
+        expect(event[2]).to.equal(Merge({}, bulkResult[2], optionalFields));
+        expect(tags).to.be.a.function();
+        expect(callback).to.be.undefined();
+        tags(null);
+        done();
+      };
+
+      stream.pipe(reporter);
+      stream.push(bulkLogEventData[0]);
+      stream.push(bulkLogEventData[1]);
+      stream.push(bulkLogEventData[2]);
+      stream.push(null);
+    });
+
+    it('does nothing if buffer is empty on termination of stream', (done) => {
+      const stream = readStream();
+      const options = Merge({ threshold: 100 }, logglyOptions, optionalFields);
+      const reporter = new GoodLoggly(options);
+
+      Loggly.Loggly.prototype.log = function (event, tags, callback) {
+        Code.fail('We should never get here');
+      };
+
+      stream.pipe(reporter);
+      stream.push(null);
+      done();
+    });
+
+    it('resets buffer after a send', (done) => {
+      const stream = readStream();
+      const options = Merge({ threshold: 2 }, logglyOptions, optionalFields);
+      const reporter = new GoodLoggly(options);
+
+      Loggly.Loggly.prototype.log = function (event, tags, callback) {
+        expect(event).to.be.an.array();
+        expect(event).to.have.length(2);
+        expect(event[0]).to.equal(Merge({}, bulkResult[0], optionalFields));
+        expect(event[1]).to.equal(Merge({}, bulkResult[1], optionalFields));
+        expect(tags).to.be.a.function();
+        expect(callback).to.be.undefined();
+        tags(null);
+      };
+
+      stream.pipe(reporter);
+      stream.push(bulkLogEventData[0]);
+      stream.push(bulkLogEventData[1]);
+      setImmediate(() => {
+        stream.push(bulkLogEventData[2]);
+        expect(reporter._buffer).to.have.length(1);
+        done();
+      });
     });
   });
 });
